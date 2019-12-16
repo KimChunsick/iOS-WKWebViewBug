86157cd3a742e3d9db8865d0a0955e65bf2c5d06
Kim Deokwon <codevillain@naver.com>
2019-12-16 22:59:48 +0900

    상수용 구조체 추가
    Signed-off-by: Kim Deokwon <codevillain@naver.com>

---
+++ WKWebViewBug/Util/Constant/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WKWebViewBug
+//
+//  Created by 김덕원 on 16/12/2019.
+//  Copyright © 2019 김덕원. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let url = "https://addpipe.com/html-media-capture-demo/"
+}
